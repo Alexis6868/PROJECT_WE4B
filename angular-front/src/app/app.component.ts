6c71c58c8a167d7f23e292af932d2f5d4538d14d
Spitlaser <108297447+Spitlaser@users.noreply.github.com>
2026-06-12 22:46:52 +0200

    Ajout de galérien pour avoir un footer de facon vanilla

--- angular-front/src/app/app.component.ts
+++ angular-front/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { ApiService } from './services/api.service';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
