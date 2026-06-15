7d0e47feb19d4495a1367abb56b1ea651d0c72e9
Yann FC <108297447+Spitlaser@users.noreply.github.com>
2026-06-15 10:41:46 +0200

    style appliquer pour le login et register -> attaque le catalogue mtn

--- angular-front/src/app/pages/register/register.component.ts
+++ angular-front/src/app/pages/register/register.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, RouterLinkActive],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
 registerForm: FormGroup;
